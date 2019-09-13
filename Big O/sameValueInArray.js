dabe4c92dc8e90191e7ba557bf17c570cb52b2cd
tanisha2398 <tanishanegipro@gmail.com>
2019-09-13 16:19:10 +0530

    O(a+b) complexity  array containing common item

--- Big O/sameValueInArray.js
+++ Big O/sameValueInArray.js
@@ -1,5 +1,5 @@
 const arr1 = ["a", "b", "c", "x"];
-const arr2 = ["z", "y", "x"];
+const arr2 = ["z", "y", "c"];
 function containCommonItem(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
@@ -9,4 +9,21 @@ function containCommonItem(arr1, arr2) {
     }
   }
 }
-containCommonItem(arr1, arr2);
+containCommonItem(arr1, arr2); //O(a*b)
+function containCommonItem2(arr1, arr2) {
+  let map = {};
+  for (let i = 0; i < arr1.length; i++) {
+    if (!map[arr1[i]]) {
+      const item = arr1[i];
+      map[item] = true;
+    }
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+}
+containCommonItem2(arr1, arr2);
